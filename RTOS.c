d28f1be1381d1c3cfbd2f8bc99746964556e6774
Jose Nunez <jose.nunez@advisiondevelopment.com>
2020-10-09 19:40:06 -0600

    MATRIX

--- RTOS.c
+++ RTOS.c
@@ -10,12 +10,12 @@ int main()
     for (int c = 0; c < M_ROW; c++)
     {
         for (int d = 0; d < M_COL; d++)
-        {            
+        {
             printf("%d\t", MAZE_MATRIX[c][d]);
         }
         printf("\n");
     }
-   // return 0;
+    // return 0;
     if (!al_init())
     {
         printf("couldn't initialize allegro\n");
